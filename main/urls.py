95f1c9adb38f306bb79e7f3d33e8965f23f3366c
Sansiel <deviler.san@yandex.ru>
2019-10-25 22:26:51 +0400

    urlToRoute

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,18 @@
-from django.urls import path
+# from django.urls import path
+
+# from . import views
+
+# urlpatterns = [
+#     path('author', views.AuthorViewSet.as_view()),
+#     path('book', views.BookViewSet.as_view()),
+# ]
+
+from rest_framework import routers
 
 from . import views
 
-urlpatterns = [
-    path('author', views.AuthorViewSet.as_view()),
-    path('book', views.BookViewSet.as_view()),
-]
+router = routers.SimpleRouter()
+router.register(r'author', views.AuthorViewSet, basename='author')
+router.register(r'book', views.BookViewSet, basename='book')
+
+urlpatterns = router.urls
